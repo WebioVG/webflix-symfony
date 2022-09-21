f088b6fd7b1be64e5644544e6850c85b1e2a2de7
Kévin Kowalsky <kevin.kowalsky1@gmail.com>
2022-09-21 17:05:18 +0200

    get all movies

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,12 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MovieController extends AbstractController
 {
+    public $doctrine;
+    public $entityManager;
+    public $movies;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+        $this->entityManager = $doctrine->getManager();
+        $this->movies = $doctrine->getRepository(Movie::class)->findAll();
+        dump($this->movies);
+    }
+
     #[Route('/categories', name: 'categories')]
     public function categories(): Response
     {
@@ -17,6 +31,8 @@ class MovieController extends AbstractController
     #[Route('/movies/list', name: 'movie_list')]
     public function list(): Response
     {
-        return $this->render('movie/list.html.twig');
+        return $this->render('movie/list.html.twig', [
+            'movies' => $this->movies
+        ]);
     }
 }
